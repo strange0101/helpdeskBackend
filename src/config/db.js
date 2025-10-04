1a1255ed4aef3a770c669e591b0cb5fa74a7e212
Aryan Sahu <121814870+strange0101@users.noreply.github.com>
2025-10-04 16:52:01 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -8,3 +8,7 @@ export const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL,
   ssl: { rejectUnauthorized: false },
 });
+
+pool.connect()
+  .then(() => console.log('Connected to Supabase DB'))
+  .catch((err) => console.error('Supabase DB connection error:', err));
